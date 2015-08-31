0302e12240ebd43fe38e497d974c199106add1ba
Rik Howard <lrbh64@gmail.com>
2015-08-31 17:40:42 +0100

    intro intellij examples

---
+++ hello-intellij/maven-jar-main/src/main/java/hello/intellij/maven/jar/main/Main.java
@@ -0,0 +1,14 @@
+
+package hello.intellij.maven.jar.main;
+
+public class Main {
+
+    public static void main (String [] args)
+    throws InterruptedException {
+        while (true) {
+            System.out.print (".");
+            Thread.sleep (3000);
+        }
+    }
+
+}
